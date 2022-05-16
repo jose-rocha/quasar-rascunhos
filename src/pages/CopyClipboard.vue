0daf4516f4e0cc76b791713650283c7232ab66d5
José Rocha <josemarlerocha@hotmail.com>
2022-05-15 21:06:13 -0300

    adicionando watch para observar a mudança da variável count

--- src/pages/CopyClipboard.vue
+++ src/pages/CopyClipboard.vue
@@ -5,10 +5,19 @@
     <q-btn @click="copyText" label="Copy" />
 
   </div>
+
+  <div class="flex column justify-center q-mt-md">
+    <h5 class="text-center">Utilizando watch</h5>
+    <span class="text-center">Valor de count {{count}}</span>
+
+    <q-btn label="add" @click="() => count++" />
+    <span v-if="msg" class="text-red text-center q-mt-sm">count chegou no limite</span>
+  </div>
 </q-page>
 </template>
 
 <script setup >
+import { ref, watch } from 'vue';
 
 const lorem = 'lorem ipsum';
 
@@ -19,4 +28,14 @@ const copyText = () => {
     window.console.log(e);
   }
 };
+
+const count = ref(0);
+const msg = ref(false);
+
+watch(() => count.value, () => {
+  if (count.value === 5) {
+    // alert(`count é ${count.value}`);
+    msg.value = true;
+  }
+});
 </script>
